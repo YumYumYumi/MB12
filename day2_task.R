e90458f8c8ee7a5b44dbde43a3086de33c723ef5
YumYumYumi <hyunminkang88@gmail.com>
2020-10-30 13:03:18 +0100

    how to rebase in Terminal, day2 task start (nachdem Umzug mach weiter!!)

---
+++ day2_task.R
@@ -0,0 +1,2 @@
+Eprod <- read.csv("data/day2_data_energy_prod_EU_2020-08-03_2020-08-09.csv")
+head(Eprod)
---
+++ gitPull.R
@@ -0,0 +1,17 @@
+#cd git_environment
+#cd MB12
+#git remote add upstream http://github.com/16EAGLE/MB12.git
+# git fetch upstream
+
+#git checkout master
+#git rebase upstream/master
+#git push -f origin master
+#git commit -m "before rebase"
+
+#git add .
+#git commit -m "before rebase"
+#git push origin master
+#git checkout master
+#git rebase upstream/master
+#git remote add upstream http://github.com/16EAGLE/MB12.git
+#git rebase upstream/master
